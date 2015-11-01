e16929850dc5b826f7e29d4a2fe9bcec3d2ea01e
jasminejeane <jasminejeane@gmail.com>
2015-11-01 01:54:36 -0700

    mid week update

--- temperature-c/temp-c.js
+++ temperature-c/temp-c.js
@@ -1,12 +1,17 @@
 
 var tempF = document.querySelector('.temp-f');
 var tempC = document.querySelector('.temp-c');
+var buttonOne = document.querySelector('.c_to_f');
+var buttonTwo = document.querySelector('.f_to_c');
+// var body = document.querySelector("body");
 
-tempF.addEventListener('keyup', calcFtoC);
-tempC.addEventListener('keyup', calcCtoF);
+// tempF.addEventListener('keyup', calcFtoC);
+// tempC.addEventListener('keyup', calcCtoF);
+buttonOne.addEventListener('click', calcCtoF);
+buttonTwo.addEventListener('click', calcFtoC);
 
 
-
+$(".fc").hide().show("slow");
 
  function calcCelciusToFahrenheit(c){
         var f = c * 9/5 + 32
@@ -19,8 +24,10 @@ tempC.addEventListener('keyup', calcCtoF);
         var c = (f-32) * (5/9)
         return c;
 
-      }  
-   
+      } 
+
+
+
 
 function calcFtoC(event) {
   if (tempF.value.length > 0) {
@@ -30,6 +37,7 @@ function calcFtoC(event) {
   }
 }
 
+
 function calcCtoF(event) {
   if (tempC.value.length > 0) {
     tempF.value = calcCelciusToFahrenheit(tempC.value)
@@ -37,3 +45,21 @@ function calcCtoF(event) {
     tempF.value = '';
   }
 }
+
+
+
+// function calcFtoC(event) {
+//   if (tempF.value.length > 0) {
+//     tempC.value = calcFahrenheitToCelcius(tempF.value)
+//   } else {
+//     tempC.value = '';
+//   }
+// }
+
+// function calcCtoF(event) {
+//   if (tempC.value.length > 0) {
+//     tempF.value = calcCelciusToFahrenheit(tempC.value)
+//   } else {
+//     tempF.value = '';
+//   }
+// }
